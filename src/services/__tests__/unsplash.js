a89f5ad103b2dfc8e73821174f4e836104c5744c
olliebrownlow <olliebrownlow@gmail.com>
2019-10-08 22:46:00 +0100

    adds tests and mocks folders, tests axios get call with mockAxios

---
+++ src/services/__tests__/unsplash.js
@@ -0,0 +1,22 @@
+import mockAxios from 'axios';
+import unsplash from '../unsplash';
+
+it('calls axios and returns images', async () => {
+
+  // setup
+  mockAxios.get.mockImplementationOnce(() =>
+    Promise.resolve({
+      data: {
+        results: ["cute.jpg"]
+      }
+    })
+  );
+
+  // work
+  const images = await unsplash("kittens");
+
+  // assertions/expects
+  expect(images).toEqual(["cute.jpg"]);
+  expect(mockAxios.get).toHaveBeenCalledTimes(1);
+  expect(mockAxios.get).toHaveBeenCalledWith("https://api.unsplash.com/search/photos", {"params": {"client_id": undefined, "query": "kittens"}});
+});
